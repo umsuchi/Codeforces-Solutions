ca4072b05dd4ce394429cbcf18d6dc79fd507154
umsuchi <ummulmomeninsuchi@gmail.com>
2025-06-06 13:55:50 +0600

    Sum

---
+++ 1742A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i = 0 ; i < n ; i++){
+        int a , b , c;
+        cin >> a >> b >> c;
+        if((a + b) == c || (a + c) == b || (b + c) == a) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+
+    return 0;
+}
