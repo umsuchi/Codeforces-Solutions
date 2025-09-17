f28d5daf1476b4b6a966a018ed2802d2c7094e7d
umsuchi <ummulmomeninsuchi@gmail.com>
2025-09-17 11:19:51 +0600

    Plus One on the Subset

---
+++ 1624A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        vector<long>a(n);
+        
+        for(int i=0; i<n; i++) cin >> a[i];
+
+        sort(a.begin(),a.end());
+
+        cout << a[n-1] - a[0] << endl;
+    }
+
+    return 0;
+}
