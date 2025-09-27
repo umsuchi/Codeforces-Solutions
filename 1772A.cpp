0ef73e795c803c10ad77de20694df6181a2536fc
umsuchi <ummulmomeninsuchi@gmail.com>
2025-09-27 14:54:51 +0600

    A+B?

---
+++ 1772A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t; cin >> t;
+    while(t--){
+        string s; cin >> s;
+
+        cout << s[0] - '0' + s[2] - '0' << endl;
+    }
+
+    return 0;
+}
