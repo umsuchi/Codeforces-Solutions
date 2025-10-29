c9d6525faa7053ea1ee15d868c0377d2dada9d8b
umsuchi <ummulmomeninsuchi@gmail.com>
2025-10-29 09:26:44 +0600

    Square?

---
+++ 2167A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t; cin >> t;
+
+    while(t--){
+        int a, b, c, d; cin >> a >> b >> c >> d;
+        if(a == b && b == c && c == d) cout << "Yes" << endl;
+        else cout << "No" << endl ;
+    }
+
+    return 0 ;
+}
